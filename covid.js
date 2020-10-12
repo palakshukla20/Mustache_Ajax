70ab600d03790ba3f7607b36f16985759683b8be
palakshukla20 <70757919+palakshukla20@users.noreply.github.com>
2020-10-12 10:06:22 +0530

    "mustache assignment"

--- covid.js
+++ covid.js
@@ -6,7 +6,7 @@ Promise.all([
     return response.json();
   }));
 }).then(function (data) {
-  showData(data[0], data[1])
+  main(data[0], data[1])
 }).catch(function (error) {
   console.log(error);
 });
@@ -15,8 +15,8 @@ $(document).ready(function () {
 });
 
 var result = []
-var data_obj = {}
 var object = {}
+var total =[]
 var district = ["Madurai", "Ganjam", "Alappuzha", "Mumbai", "Chennai"]
 var state_name = ["Andhra Pradesh", "Karnataka", "Gujarat", "West Bengal", "lakdakh"]
 var sts_list = document.getElementById("state_list");
@@ -38,31 +38,16 @@ for (var x = 0; x < state_name.length; x++) {
   sts_list.appendChild(ele)
 }
 
-a = []
-function merge(result, array, tt) {
-  abz = {}
-  var t_active = 0
-  var code = Object.getOwnPropertyNames(array);
-  dis_array = array[code];
-  for (x in code) {
-    z = code[x];
-    for (y in result) {
-      if (array[z].statecode == result[y].state_array ) {
-        active = 0
-        result[y].state_name = code[x]
-        district_name = array[z].districtData
-        for (x in district_name) {
-          active += district_name[x].active
-        }
-        result[y].active = active;
-        t_active += active
-        result[y].active_ratio = ((active*100)/result[y].con).toFixed(1);
-        abz["abc"] = array[z].districtData;
-      }
+function hideSearch() {
+  var ele = document.getElementById("hide_con");
+  var input = document.getElementById("input");
+  window.onclick = function (event) {
+    if (event.target == input) {
+      ele.style.display = "inline-flex";
+    } else {
+      ele.style.display = "none";
     }
-    tt[0].active = t_active.toLocaleString()
   }
-  console.log(abz)
 }
 
 function convert_rupee(amount) {
@@ -78,71 +63,75 @@ function convert_rupee(amount) {
     return 0;
   }
 }
-var tt =[]
-function showData(data, array) {
+
+function merge(result, array, total) {
+  var district    = {}
+  var totalActive = 0
+  var stateName   = Object.getOwnPropertyNames(array);
+  for (var x  in  stateName) {
+    state = stateName[x];
+    for (var y in result) {
+      if (array[state].statecode == result[y].state_array) {
+        active = 0
+        result[y].state_name = stateName[x]
+        district_name = array[state].districtData
+        for (var x in district_name) {
+          active += district_name[x].active
+        }
+        result[y].active         = active;
+        totalActive             += active
+        result[y].active_ratio   = ((active*100)/result[y].con).toFixed(1);
+        district["districtData"] = array[state].districtData;
+      }
+    }
+    total[0].active = totalActive.toLocaleString()
+  }
+}
+
+function showData(stateData,state,obj) {
+  obj["state_array"]    = state;
+  obj["con"]            =  stateData.total.confirmed
+  obj["confirmed"]      = stateData.total.confirmed.toLocaleString();
+  obj["recovered"]      = stateData.total.recovered.toLocaleString();
+  obj["population"]     = convert_rupee(stateData.meta.population);
+  obj["tested"]         = convert_rupee(stateData.total.tested);
+  obj["other"]          = stateData.total.other;
+  obj["recovery-ratio"] = ((stateData.total.recovered * 100) / stateData.total.confirmed).toFixed(1);
+  obj["case-fatality"]  = ((stateData.total.deceased * 100) / stateData.total.confirmed).toFixed(1);
+  if (stateData.total.deceased) {
+    obj["deceased"] = stateData.total.deceased.toLocaleString();
+  }
+  if (stateData.meta.notes) {
+    obj["notes"] = stateData.meta.notes;
+  }
+  if (stateData.delta) {
+    obj["delta-confirmed"] = stateData.delta.confirmed;
+    obj["delta-recovered"] = stateData.delta.recovered;
+    obj["delta-deceased"]  = stateData.delta.deceased;
+  }
+  return obj;
+}
+function main(data, array) {
   var stateCode = Object.getOwnPropertyNames(data);
-  for (x in stateCode) {
+  for (var x in stateCode) {
     var obj       = {}
     var state     = stateCode[x] 
-   
     var stateData = data[state]
     if (state == "TT") {
-      obj["state_name"]      = "India";
-      obj["confirmed"]       = stateData.total.confirmed.toLocaleString();
-      obj["recovered"]       = stateData.total.recovered.toLocaleString();
-      obj["population"]      = convert_rupee(stateData.meta.population);
-      obj["tested"]          = convert_rupee(stateData.total.tested);
-      obj["recovery-ratio"]  = ((stateData.total.recovered * 100) / stateData.total.confirmed).toFixed(1);
-      obj["case-fatality"]   = ((stateData.total.deceased * 100) / stateData.total.confirmed).toFixed(1);
-      obj["deceased"]        = stateData.total.deceased.toLocaleString();
-      obj["delta-confirmed"] = stateData.delta.confirmed.toLocaleString();
-      obj["delta-recovered"] = stateData.delta.recovered.toLocaleString();
-      obj["delta-deceased"]  = stateData.delta.deceased.toLocaleString();
-      obj["other"]           = stateData.total.other;
-      tt.push(obj);
-      object["TT"] = tt;
+      showData(stateData,state, obj)
+      total.push(obj);
+      object["India"] = total;
     } else {
-        obj["state_array"]    = state;
-        obj["con"]            =  stateData.total.confirmed
-        obj["confirmed"]      = stateData.total.confirmed.toLocaleString();
-        obj["recovered"]      = stateData.total.recovered.toLocaleString();
-        obj["population"]     = convert_rupee(stateData.meta.population);
-        obj["tested"]         = convert_rupee(stateData.total.tested);
-        obj["other"]          = stateData.total.other;
-        obj["recovery-ratio"] = ((stateData.total.recovered * 100) / stateData.total.confirmed).toFixed(1);
-        obj["case-fatality"]  = ((stateData.total.deceased * 100) / stateData.total.confirmed).toFixed(1);
-        if (stateData.total.deceased) {
-          obj["deceased"] = stateData.total.deceased.toLocaleString();
-        }
-        if (stateData.meta.notes) {
-          obj["notes"] = stateData.meta.notes;
-        }
-        if (stateData.delta) {
-          obj["delta-confirmed"] = stateData.delta.confirmed;
-          obj["delta-recovered"] = stateData.delta.recovered;
-          obj["delta-deceased"]  = stateData.delta.deceased;
-        }
+      showData(stateData,state, obj)
         result.push(obj);
       }
   }
-  merge(result, array,tt)
-  object["result"] = result;
+  merge(result, array,total)
+  object["result"]  = result;
   var template_card = document.getElementById("total_data").innerHTML;
-  var text     = Mustache.render(template_card, object);
+  var text          = Mustache.render(template_card, object);
   document.getElementById("map_card").innerHTML = text;
-  var template = document.getElementById("template").innerHTML;
-  var text     = Mustache.render(template, object);
+  var template      = document.getElementById("template").innerHTML;
+  var text          = Mustache.render(template, object);
   document.getElementById("body").innerHTML = text;
-}
-
-function hideSearch() {
-  var ele = document.getElementById("hide_con");
-  var input = document.getElementById("input");
-  window.onclick = function (event) {
-    if (event.target == input) {
-      ele.style.display = "inline-flex";
-    } else {
-      ele.style.display = "none";
-    }
-  }
 }
